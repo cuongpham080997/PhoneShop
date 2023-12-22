1d6964e1280fc638d86d06671b92ae6d889ea55e
bachlo97 <cuongpt8997@gmail.com>
2023-12-22 21:26:37 +0700

    complete card

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-const listNavLink = document.querySelectorAll(".nav-link");
-for (const nav of listNavLink) {
+//? -- Header --
+document.querySelectorAll(".nav-link").forEach((nav) => {
   console.log(nav);
+
   // Lắng nghe sự kiện onclick
   nav.onclick = () => {
     // Xóa class active trước khi thêm vào
@@ -11,4 +12,29 @@ for (const nav of listNavLink) {
     // Thêm class cho thẻ được click
     nav.classList.add("nav-link__active");
   };
-}
+});
+
+//? -- Card --
+/**
+ * B1: Lấy tất cả các card
+ * B2: Lấy button show more - less
+ * B3: Gắn thuộc tính onclick - Xử lý logic
+ */
+document.querySelectorAll(".card").forEach((card) => {
+  const btn = card.querySelector("button.more");
+  const eleText = card.querySelector('.card-text span')
+
+  if(!btn) return
+
+  const text = eleText.innerText
+
+  btn.onclick = () => {
+    if (btn.innerText.toLowerCase() === "less") {
+      eleText.innerHTML = eleText.innerText.slice(0,20) + "..."
+      btn.innerHTML = 'more'
+    } else {
+      eleText.innerHTML = text;
+      btn.innerHTML = 'less'
+    }
+  };
+});
